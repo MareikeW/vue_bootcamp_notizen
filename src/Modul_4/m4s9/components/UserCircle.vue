87f3bca783d37abfe3ce432346334bbf74426258
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-03 11:35:18 +0200

    Code für Scrim 9

---
+++ src/Modul_4/m4s9/components/UserCircle.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card-circle">{{ initials }}</div>
+</template>
+
+<script>
+    module.exports = {
+        props: ['name'],
+        
+        computed: {
+            // Egal wie viele Namen in Card.vue eingegeben werden, 
+            // es werden immer nur die ersten beiden Initialen ausgewählt.
+            initials: function () {
+                return this.name.match(/[A-Z]/g).slice(0, 2).join('');
+            }
+        }
+    }
+</script>
+
+<style>
+    .card-circle {
+        background: linear-gradient(to right, #da4453, #89216b);
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 75px;
+        height: 75px;
+        font-size: 24px;
+        font-weight: bold;
+        border-radius: 100%;
+        padding: 5px;
+        border: 3px solid #fff;
+    }
+</style>
---
+++ src/Modul_4/m4s9/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Card from './components/Card';
+
+new Vue({
+    el: '#app',
+    
+    components: {
+        Card
+    },
+    
+    data: {
+        
+    }
+});
