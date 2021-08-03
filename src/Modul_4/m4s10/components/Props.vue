28c5891e13e223b18307b997cee2c968d523fc41
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-08-03 13:00:34 +0200

    Code für Scrim 10

---
+++ src/Modul_4/m4s10/components/Props.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Advanced Props</h1>
+        <div>Views: {{ views }}</div>
+        <div>Is published: {{ isPublished }}</div>
+        <div>{{ id }} {{ title }}</div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        // gibt die Datentypen für jeden prop an
+        props: {
+            id: Number,
+            title: String,
+            isPublished: {
+                // prop darf ein Boolean oder eine Zahl sein
+                type: [Boolean, Number],
+                default: false
+            },
+            views: Number,
+        }
+    }
+</script>
+
+<style>
+    
+</style>
